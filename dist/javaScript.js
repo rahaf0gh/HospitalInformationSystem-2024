cd272ed4dd454a22b00c5720d8f893e721c7a6f1
rahaf0gh <rahaf0alghamdi@gmail.com>
2025-06-16 00:06:01 +0300

    first commit

---
+++ dist/javaScript.js
@@ -0,0 +1,237 @@
+const Nav = document.getElementById("UlNav");
+const barBtn = document.getElementById("bar-btn");
+const xmarkBtn = document.getElementById("amark-btn");
+
+function displayMenu(){
+    Nav.style.display="block";
+    barBtn.style.display="none";
+    xmarkBtn.style.display="block";
+}
+
+function closeMenu(){
+    Nav.style.display="none";
+    barBtn.style.display="block";
+    xmarkBtn.style.display="none";
+}
+
+
+
+
+
+// .................................. 
+const isLeapYear = (year) => {
+    return (
+      (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) ||
+      (year % 100 === 0 && year % 400 === 0)
+    );
+  };
+  const getFebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28;
+  };
+  let calendar = document.querySelector('.calendar');
+  const month_names = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+  let month_picker = document.querySelector('#month-picker');
+  const dayTextFormate = document.querySelector('.day-text-formate');
+  const timeFormate = document.querySelector('.time-formate');
+  const dateFormate = document.querySelector('.date-formate');
+  
+  month_picker.onclick = () => {
+    month_list.classList.remove('hideonce');
+    month_list.classList.remove('hide');
+    month_list.classList.add('show');
+    dayTextFormate.classList.remove('showtime');
+    dayTextFormate.classList.add('hidetime');
+    timeFormate.classList.remove('showtime');
+    timeFormate.classList.add('hideTime');
+    dateFormate.classList.remove('showtime');
+    dateFormate.classList.add('hideTime');
+  };
+  
+  const generateCalendar = (month, year) => {
+    let calendar_days = document.querySelector('.calendar-days');
+    calendar_days.innerHTML = '';
+    let calendar_header_year = document.querySelector('#year');
+    let days_of_month = [
+        31,
+        getFebDays(year),
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31,
+      ];
+  
+    let currentDate = new Date();
+  
+    month_picker.innerHTML = month_names[month];
+  
+    calendar_header_year.innerHTML = year;
+  
+    let first_day = new Date(year, month);
+  
+  
+    for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
+  
+      let day = document.createElement('div');
+  
+      if (i >= first_day.getDay()) {
+        day.innerHTML = i - first_day.getDay() + 1;
+  
+        if (i - first_day.getDay() + 1 === currentDate.getDate() &&
+          year === currentDate.getFullYear() &&
+          month === currentDate.getMonth()
+        ) {
+          day.classList.add('current-date');
+        }
+      }
+      calendar_days.appendChild(day);
+    }
+  };
+  
+  let month_list = calendar.querySelector('.month-list');
+  month_names.forEach((e, index) => {
+    let month = document.createElement('div');
+    month.innerHTML = `<div>${e}</div>`;
+  
+    month_list.append(month);
+    month.onclick = () => {
+      currentMonth.value = index;
+      generateCalendar(currentMonth.value, currentYear.value);
+      month_list.classList.replace('show', 'hide');
+      dayTextFormate.classList.remove('hideTime');
+      dayTextFormate.classList.add('showtime');
+      timeFormate.classList.remove('hideTime');
+      timeFormate.classList.add('showtime');
+      dateFormate.classList.remove('hideTime');
+      dateFormate.classList.add('showtime');
+    };
+  });
+  
+  (function() {
+    month_list.classList.add('hideonce');
+  })();
+  document.querySelector('#pre-year').onclick = () => {
+    --currentYear.value;
+    generateCalendar(currentMonth.value, currentYear.value);
+  };
+  document.querySelector('#next-year').onclick = () => {
+    ++currentYear.value;
+    generateCalendar(currentMonth.value, currentYear.value);
+  };
+  
+  let currentDate = new Date();
+  let currentMonth = { value: currentDate.getMonth() };
+  let currentYear = { value: currentDate.getFullYear() };
+  generateCalendar(currentMonth.value, currentYear.value);
+  
+  const todayShowTime = document.querySelector('.time-formate');
+  const todayShowDate = document.querySelector('.date-formate');
+  
+  const currshowDate = new Date();
+  const showCurrentDateOption = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    weekday: 'long',
+  };
+  const currentDateFormate = new Intl.DateTimeFormat(
+    'en-US',
+    showCurrentDateOption
+  ).format(currshowDate);
+  todayShowDate.textContent = currentDateFormate;
+  setInterval(() => {
+    const timer = new Date();
+    const option = {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    };
+    const formateTimer = new Intl.DateTimeFormat('en-us', option).format(timer);
+    let time = `${`${timer.getHours()}`.padStart(
+        2,
+        '0'
+      )}:${`${timer.getMinutes()}`.padStart(
+        2,
+        '0'
+      )}: ${`${timer.getSeconds()}`.padStart(2, '0')}`;
+    todayShowTime.textContent = formateTimer;
+  }, 1000);
+
+// ------------------------------ animated counter
+let nums = document.querySelectorAll(".rect .No");
+let section = document.querySelector(".Numbers");
+let started = false; // Function Started ? No
+
+window.onscroll = function () {
+  if (window.scrollY <= section.offsetTop) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
+
+  // -------------------------------------------------- filter table
+  const searchFun = () => {
+    let filter = document.getElementById('myInput').value.toUpperCase();
+
+    let myTable = document.getElementById('myTable');
+
+    let tr = myTable.getElementsByTagName('tr');
+
+    let notFOund = document.getElementById('notFound');
+
+    for (var i = 0; i < tr.length; i++) {
+        let td = tr[i].getElementsByTagName('td')[1];
+
+
+        if (td) {
+           
+
+            let textvalue = td.texContent || td.innerHTML;
+
+            if (textvalue.toUpperCase().indexOf(filter) > -1) {
+
+                tr[i].style.display = "";
+
+                // console.log("indisplay empty :" , tr[i]);
+            }
+            else {
+                tr[i].style.display = "none";
+                notFOund.style.display = "block"
+                notFOund.innerHTML = "نأسف ، اسم الطبيب غير موجود"
+
+                // console.log("indisplay none :" , tr[i]);
+            }
+        }
+
+    }
+}
